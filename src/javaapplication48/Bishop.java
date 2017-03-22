5ecce150afe4b25136cd7fe2cc0f3a251ecf29bb
Stabz <dmit0038@edu.easj.dk>
2017-03-22 22:57:09 +0100

    Merge origin/master
    Conflicts:
    	src/view/kunder.java

--- src/javaapplication48/Bishop.java
+++ src/javaapplication48/Bishop.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication48;
 
 /**
@@ -11,20 +6,92 @@ package javaapplication48;
  */
 public class Bishop extends ChessPiece {
 
+    /**
+     *
+     * @param name
+     * @param isWhite
+     * @param xPosition
+     * @param yPosition
+     */
     public Bishop(String name, boolean isWhite, int xPosition, int yPosition) {
         super(name, isWhite, xPosition, yPosition);
     }
 
+    /**
+     *
+     * @param x
+     * @param y
+     * @return
+     */
     public boolean canMoveTo(int x, int y) {
         boolean canMove;
-        canMove = !true;
+        canMove = false;
+        if (super.canMove(x, y)) {
+            
+                if (Math.abs(getxPosition()-x)==Math.abs(getyPosition()-y)) 
+                {
 
-        if (getxPosition() - x == getyPosition() - y || getxPosition() - x == getyPosition() + y || getxPosition() + x == getyPosition() + y || getxPosition() + x == getyPosition() - y) {
-            if (super.canMove(x, y)) {
-                canMove = true;
-            }
+                    canMove = true;
+                }
+            
+        }
+     
+        return canMove;
+    }
+    public void moveTwo(int x, int y) {
+        String txt = "\n";
+
+        if (!isIsWhite()) {
+            txt += "Black ";
+        } else {
+            txt += "White ";
+        }
+        txt += getName() + " current possition: " + numberToLetter(getxPosition()) + getyPosition();
+
+        if (canMoveTo(x, y)==true) {
+
+            setxPosition(x);
+            setyPosition(y);
+            txt += "\nmoves to: " + numberToLetter(getxPosition()) + getyPosition();
+
+        } else {
+            txt += "\ncannot move to: " + numberToLetter(x) + y;
+        }
+        System.out.println(txt);
+    }
+
+    private String numberToLetter(int xPosition) {
+        String txt;
+
+        switch (xPosition) {
+            case 1:
+                txt = "A";
+                break;
+            case 2:
+                txt = "B";
+                break;
+            case 3:
+                txt = "C";
+                break;
+            case 4:
+                txt = "D";
+                break;
+            case 5:
+                txt = "E";
+                break;
+            case 6:
+                txt = "F";
+                break;
+            case 7:
+                txt = "G";
+                break;
+            case 8:
+                txt = "H";
+                break;
+            default:
+                txt = "out of bounds";
         }
 
-        return canMove;
+        return txt;
     }
 }
